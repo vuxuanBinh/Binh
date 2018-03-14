021edf09caee0395c10448df11a8134cd174e663
vuxuanBinh <vuxuanbinh934@gmail.com>
2018-03-14 16:34:30 +0700

    nop code
    nop code

---
+++ baitap1.php
@@ -0,0 +1,13 @@
+<?php
+ $Chuoi = "Chao mung ban den voi lap trinh wed 1<br>";
+echo substr($Chuoi,22);
+echo substr($Chuoi,0,-36),"<BR>";
+echo substr($Chuoi,5,-32),"<BR>";
+echo substr($Chuoi,10,-28),"<BR>";
+echo substr($Chuoi,14,-24),"<BR>";
+echo substr($Chuoi,18,-20),"<BR>";
+echo substr($Chuoi,22,-16),"<BR>";
+echo substr($Chuoi,26,-10),"<BR>";
+echo substr($Chuoi,32,-6),"<BR>";
+echo substr($Chuoi,36,-1),"<BR>";
+?>
---
+++ baitap2.php
@@ -0,0 +1,9 @@
+<html>
+    
+    <h1>BAI TAP DINH DANG NGAY</h1>
+    <h3>NHAP NGAY </h3>
+    <from>binh</from>
+<?php
+
+?>
+</html>
